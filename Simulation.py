72e52a603a184c9559ae15e1ca56e9ae55d6fd9a
Paul Sohn <pksohn@gmail.com>
2017-08-23 08:07:53 -0700

    Turn on refiner, turn off scheduled events

--- Simulation.py
+++ Simulation.py
@@ -22,8 +22,8 @@ orca.run([
     "households_relocation",
     "jobs_transition",
     "jobs_relocation",
-    # "scheduled_demolition_events",
-    # "scheduled_development_events",
+    "scheduled_demolition_events",
+    "scheduled_development_events",
     "feasibility",
     "set_target_vacancies",
     "residential_developer",
@@ -33,7 +33,7 @@ orca.run([
     "hlcm_simulate",  # households location choice
     "elcm_simulate",  # employment location choice
     # "government_jobs_scaling_model",
-    "refiner",
+    # "refiner",
     # "gq_model", Fixme: we have new data so need new approach
     # "travel_model", Fixme: on hold
     # "housing_value_update", Fixme: maybe we don't need
